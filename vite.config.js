3cad0927067304cf90e1df722dfb0129c878713f
savyasachi bhat <savyasachibhat52@gmail.com>
2025-06-13 11:28:15 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
  
   plugins: [react()],
+    base: './', // Ensures relative paths
+  server: {
+    historyApiFallback: true, // Fixes 404 on direct navigation
+  },
 })
