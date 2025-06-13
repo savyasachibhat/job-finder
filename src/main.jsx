a6d0e870c42b666b391306050fc96f29d1200787
savyasachi bhat <savyasachibhat52@gmail.com>
2025-06-13 10:42:08 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// )
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>,
 )
